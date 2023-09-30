6e4c1430822049a6b379aa7b4a4724792686df2c
Vanessa Josy <vanessa.josy@icloud.com>
2023-09-30 11:23:25 +0200

    Create variabel city

--- src/app.js
+++ src/app.js
@@ -38,6 +38,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "8dbc36ba471db85fd697a87348283258";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
